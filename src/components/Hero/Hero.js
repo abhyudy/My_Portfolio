31645621be4cb11f6076936b9b2bbcc5b94922a2
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-08-04 23:26:55 +0530

    Initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+//Styles section
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => <div>Hero</div>;
+
+export default Hero;
